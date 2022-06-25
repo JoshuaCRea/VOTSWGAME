e510bd35da74d9d07374ff3ad7e964551c3eac50
Joshua Rea <josh.rea@gmail.com>
2022-06-25 17:53:39 -0400

    PC stats, hometown attribute, injured attribute

--- player_character.py
+++ player_character.py
@@ -2,8 +2,10 @@ class PlayerCharacter:
     def __init__(self):
         self.hp = 5
         self.standing = True
+        self.injured = False
 
     def receive_damage(self, amount):
         self.hp -= amount
         if self.hp <= 0:
             self.standing = False
+            self.injured = False
