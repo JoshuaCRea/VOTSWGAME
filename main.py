dadbd71b6f7b317bb5764f763d5699f22c7d5d44
Joshua Rea <josh.rea@gmail.com>
2022-06-25 22:36:14 -0400

    character creation basics working

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from player_character import PlayerCharacter
 
+print("Welcome to The Valley of the Silver Wolf!")
+name = input("What is your name? ")
+hometown = input("Which town of the Valley is your hometown? Blackstone, Fangmarsh, Leap-Creek, Pouch, or Underclaw? ")
 
-soomin = PlayerCharacter()
+name = PlayerCharacter(hometown)
 
-print(soomin.hp)
-print(soomin.standing)
-soomin.receive_damage(4)
-print(soomin.hp)
-print(soomin.standing)
-soomin.receive_damage(1)
-print(soomin.hp)
-print(soomin.standing)
+print(name.hp)
+print(name.standing)
+name.receive_damage(4)
+print(name.hp)
+print(name.standing)
+name.receive_damage(1)
+print(name.hp)
+print(name.standing)
+
+print(name.STA)
+print(name.POW)
+print(name.AGI)
+print(name.CHI)
+print(name.WIT)
--- player_character.py
+++ player_character.py
@@ -1,11 +1,19 @@
+from config import HOMETOWNS
+
 class PlayerCharacter:
-    def __init__(self):
+    def __init__(self, hometown):
         self.hp = 5
         self.standing = True
         self.injured = False
+        self.STA = HOMETOWNS[hometown]["STA"]
+        self.POW = HOMETOWNS[hometown]["POW"]
+        self.AGI = HOMETOWNS[hometown]["AGI"]
+        self.CHI = HOMETOWNS[hometown]["CHI"]
+        self.WIT = HOMETOWNS[hometown]["WIT"]
 
     def receive_damage(self, amount):
         self.hp -= amount
         if self.hp <= 0:
             self.standing = False
-            self.injured = False
+            self.injured = True
+    
