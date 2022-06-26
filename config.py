e877eef3064b596595c07346dc85aaffe6b10cde
Joshua Rea <josh.rea@gmail.com>
2022-06-25 22:36:55 -0400

    added config

---
+++ config.py
@@ -0,0 +1,87 @@
+HOMETOWNS = {
+    "Blackstone" : {
+        "STA" : 2,
+        "POW" : 0,
+        "AGI" : 0,
+        "CHI" : 0,
+        "WIT" : 1,
+    },
+    "Fangmarsh" : {
+        "STA" : 1,
+        "POW" : 2,
+        "AGI" : 0,
+        "CHI" : 0,
+        "WIT" : 0,
+    },
+    "Leap-Creek" : {
+        "STA" : 0,
+        "POW" : 0,
+        "AGI" : 1,
+        "CHI" : 2,
+        "WIT" : 0,
+    },
+    "Pouch" : {
+        "STA" : 0,
+        "POW" : 0,
+        "AGI" : 0,
+        "CHI" : 1,
+        "WIT" : 2,
+    },
+    "Underclaw" : {
+        "STA" : 0,
+        "POW" : 1,
+        "AGI" : 2,
+        "CHI" : 0,
+        "WIT" : 0,
+    },
+}
+
+
+
+# HOMETOWNS = {
+#     1 : {
+#     "Blackstone" : {
+#         "STA" : 2,
+#         "POW" : 0,
+#         "AGI" : 0,
+#         "CHI" : 0,
+#         "WIT" : 1,
+#     },
+#     },
+#     2 : {
+#     "Fangmarsh" : {
+#         "STA" : 1,
+#         "POW" : 2,
+#         "AGI" : 0,
+#         "CHI" : 0,
+#         "WIT" : 0,
+#     },
+#     },
+#     3 : {
+#     "Leap-Creek" : {
+#         "STA" : 0,
+#         "POW" : 0,
+#         "AGI" : 1,
+#         "CHI" : 2,
+#         "WIT" : 0,
+#     },
+#     },
+#     4 : {
+#     "Pouch" : {
+#         "STA" : 0,
+#         "POW" : 0,
+#         "AGI" : 0,
+#         "CHI" : 1,
+#         "WIT" : 2,
+#     },
+#     },
+#     5 : {
+#     "Underclaw" : {
+#         "STA" : 0,
+#         "POW" : 1,
+#         "AGI" : 2,
+#         "CHI" : 0,
+#         "WIT" : 0,
+#     },
+#     },
+# }
