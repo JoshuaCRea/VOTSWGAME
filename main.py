0738fa2b93b382d0c64c6243e4a69db12c8ff220
Jed Duffey <jed@experimentaldigitalresearch.com>
2022-06-28 15:01:45 -0400

    WIP

--- main.py
+++ main.py
@@ -3,25 +3,32 @@ from quest_card import QuestCard
 import random
 
 
-def generate_five_quest_cards():
-    return [QuestCard() for _ in range(5)]
+def generate_twenty_quest_cards():
+    return [QuestCard() for _ in range(20)]
+
+
+def generate_five_quest_cards(cards):
+    random.shuffle(cards)
+    return cards[:5]
 
 
 def display_info_of_quest_card(quest_card):
     print(f'Chosen Card:\n{quest_card}')
 
 
-def display_pertinent_quest_card_info(quest_cards):
-    for card in quest_cards:
+def display_pertinent_quest_card_info(cards):
+    for card in cards:
         abilities = [card.step_one["abilities"][0], card.step_one["abilities"][1], card.step_two["abilities"][0], card.step_two["abilities"][1]]
         print(f'{card.name}, {card.difficulty_class}, {abilities}, {card.rewards}')
 
 
 if __name__ == '__main__':
     pc = PlayerCharacter("Blackstone")
-    print(pc); print()
+    print(pc)
 
-    five_quest_cards = generate_five_quest_cards(); print()
+    twenty_quest_cards = generate_twenty_quest_cards(); print()
+    display_pertinent_quest_card_info(twenty_quest_cards); print()
+    five_quest_cards = generate_five_quest_cards(twenty_quest_cards); print()
     display_pertinent_quest_card_info(five_quest_cards); print()
 
     quest_card = random.choice(five_quest_cards)
