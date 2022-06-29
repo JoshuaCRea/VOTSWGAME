85fa801cb6cc534b76c8e44ee735d61473711472
Jed Duffey <jed@experimentaldigitalresearch.com>
2022-06-29 11:58:20 -0400

    Refactor main to only new up a GameManager(); next step is to implement an observer that will be responsible for printing events to console

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-from player_character import PlayerCharacter
-import random
-
-from school import School
+from game_manager import GameManager
 
 
 if __name__ == '__main__':
-    pc = PlayerCharacter("Blackstone")
-    print(pc)
-    school = School("School of Hong Quan")
-
-    five_quest_cards = school.present_quest_cards()
-    for card in five_quest_cards:
-        print(card.get_preview_of_card())
-
-    quest_card = random.choice(five_quest_cards)
-    print(quest_card.__repr__) # TODO remove
-    print(quest_card.get_preview_of_card())
-
-    pc.embark_quest(quest_card); print()
-    print(f'Rep rank: {pc.rep_rank}'); print(f'Techniques: {pc.techniques}')
+    gm = GameManager()
