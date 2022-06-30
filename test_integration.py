49fcaaebaa56391abb613a4174824f1de6fa1209
Jed Duffey <jed@experimentaldigitalresearch.com>
2022-06-30 14:53:05 -0400

    Tests for quest wins and losses

--- test_integration.py
+++ test_integration.py
@@ -1,4 +1,3 @@
-from cmath import e
 from game_manager import GameManager
 
 
@@ -26,4 +25,27 @@ def test_player_wins():
     assert player.techniques[0] == expected_technique
     assert player.abilities["POW"] == player_stat_before_quest + 1
     assert len(gm.schools["Blackstone"].quest_cards) == 19
+    assert player.rep_rank == 6
+
+
+def test_player_loses():
+    gm = GameManager()
+
+    player = gm.players[0]
+
+    # Manually set RepRank to 5 to ensure that no cards are filtered (so as not to throw exception internally)
+    player.rep_rank = 5
+
+    drawn_cards = gm.draw_quest_cards(player)
+
+    selected_card = gm.select_card(drawn_cards)
+
+    # Manually set DC to 50 so that Player always loses
+    selected_card.difficulty_class = 50
+
+    gm.execute_quest(player, selected_card)
+
+    assert len(player.techniques) == 0
+    assert len(gm.schools["Blackstone"].quest_cards) == 20
+    assert player.rep_rank == 4
 
