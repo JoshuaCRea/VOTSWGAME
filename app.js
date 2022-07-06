65dd830a728ec06268ae7dfc154b4cdb816d4ede
Joshua Rea <josh.rea@gmail.com>
2022-07-06 11:32:35 -0400

    travel to towns clockwise

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+var townIds = ["#townA", "#townB", "#townD", "#townC"];
+var currentTownIndex = 0;
+
 $(".move-button").click(function () {
-    $("#townB").css('background-color', '#f8cf95');
+    if (currentTownIndex == 4) {
+        currentTownIndex = 0
+    }
+    var currentTownId = townIds[currentTownIndex];
+    currentTownIndex++;
+    $(".town").css('background-color', '#ffffff');
+    $(currentTownId).css('background-color', '#f8cf95');
 })
