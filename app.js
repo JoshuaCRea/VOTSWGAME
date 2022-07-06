0a7b49fc5906686651ea155dd2fbf7cdc82007c1
Joshua Rea <josh.rea@gmail.com>
2022-07-06 14:37:26 -0400

    rfactor

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const LOCATION_IDS = ["#townA", "#roadAB", "#townB", "#roadBD", "#townD", "#roadCD", "#townC", "#roadAC"];
 const OCCUPIED_LOCATION_COLOR = '#f8cf95';
 const UNOCCUPIED_LOCATION_COLOR = '#ffffff';
+const CW_DIR_VALUE = 1
+const CCW_DIR_VALUE = -1
 
 var currentLocationIndex = 0;
 
 $(LOCATION_IDS[currentLocationIndex]).css('background-color', OCCUPIED_LOCATION_COLOR);
+
+function updateLocationIndex(directionValue) {
+    currentLocationIndex = currentLocationIndex + directionValue;
+    if (currentLocationIndex == LOCATION_IDS.length) {
+        currentLocationIndex = 0;
+    }
+    if (currentLocationIndex == -1) {
+        currentLocationIndex = LOCATION_IDS.length - 1;
+    }
+}
 
 function updateLocationColors(occupiedLocationId) {
     $(".town").css('background-color', UNOCCUPIED_LOCATION_COLOR);
@@ -12,18 +24,12 @@ function updateLocationColors(occupiedLocationId) {
     $(occupiedLocationId).css('background-color', OCCUPIED_LOCATION_COLOR);
 }
 
-$("#moveCcwButton").click(function () {
-    currentLocationIndex--;
-    if (currentLocationIndex == -1) {
-        currentLocationIndex = LOCATION_IDS.length - 1;
-    }
+$("#moveCwButton").click(function () {
+    updateLocationIndex(CW_DIR_VALUE)
     updateLocationColors(LOCATION_IDS[currentLocationIndex]);
 })
 
-$("#moveCwButton").click(function () {
-    currentLocationIndex++;
-    if (currentLocationIndex == LOCATION_IDS.length) {
-        currentLocationIndex = 0;
-    }
+$("#moveCcwButton").click(function () {
+    updateLocationIndex(CCW_DIR_VALUE)
     updateLocationColors(LOCATION_IDS[currentLocationIndex]);
 })
