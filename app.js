f009fb85f668ba98b4280730c6c180b28305798b
Joshua Rea <josh.rea@gmail.com>
2022-07-06 13:37:23 -0400

    refactor

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-var townIds = ["#townA", "#townB", "#townD", "#townC"];
-var currentTownIndex = 0;
-var currentTownId = townIds[currentTownIndex];
-$(currentTownId).css('background-color', '#f8cf95');
+const TOWN_IDS = ["#townA", "#townB", "#townD", "#townC"];
+const OCCUPIED_TOWN_COLOR = '#f8cf95';
+const UNOCCUPIED_TOWN_COLOR = '#ffffff'
 
+var currentTownIndex = 0;
+var currentTownId = TOWN_IDS[currentTownIndex];
+
+$(currentTownId).css('background-color', OCCUPIED_TOWN_COLOR);
+
+function updateTownColors(townId) {
+    $(".town").css('background-color', UNOCCUPIED_TOWN_COLOR);
+    $(townId).css('background-color', OCCUPIED_TOWN_COLOR);
+}
 
 $("#moveCcwButton").click(function () {
-    console.log(`current town index is: ${currentTownIndex}`);
     currentTownIndex--;
     if (currentTownIndex == -1) {
-        currentTownIndex = 3
+        currentTownIndex = TOWN_IDS.length - 1;
     }
-    var currentTownId = townIds[currentTownIndex];
-    console.log(`current town index is: ${currentTownIndex}`);
-    $(".town").css('background-color', '#ffffff');
-    $(currentTownId).css('background-color', '#f8cf95');
+    var currentTownId = TOWN_IDS[currentTownIndex];
+    updateTownColors(currentTownId);
 })
 
 $("#moveCwButton").click(function () {
-    console.log("I just clicked the move CW button");
     currentTownIndex++;
-    console.log(`current town index is: ${currentTownIndex}`);
-    if (currentTownIndex == 4) {
-        currentTownIndex = 0
+    if (currentTownIndex == TOWN_IDS.length) {
+        currentTownIndex = 0;
     }
-    var currentTownId = townIds[currentTownIndex];
-    console.log(`current town index is: ${currentTownIndex}`);
-    $(".town").css('background-color', '#ffffff');
-    $(currentTownId).css('background-color', '#f8cf95');
+    var currentTownId = TOWN_IDS[currentTownIndex];
+    updateTownColors(currentTownId);
 })
