d4e8aed9422e448717acaed945ddfec8192fdf87
Joshua Rea <josh.rea@gmail.com>
2022-07-06 14:24:44 -0400

    added roads, refactored

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-const TOWN_IDS = ["#townA", "#townB", "#townD", "#townC"];
-const OCCUPIED_TOWN_COLOR = '#f8cf95';
-const UNOCCUPIED_TOWN_COLOR = '#ffffff'
+const LOCATION_IDS = ["#townA", "#roadAB", "#townB", "#roadBD", "#townD", "#roadCD", "#townC", "#roadAC"];
+const OCCUPIED_LOCATION_COLOR = '#f8cf95';
+const UNOCCUPIED_LOCATION_COLOR = '#ffffff';
 
-var currentTownIndex = 0;
-var currentTownId = TOWN_IDS[currentTownIndex];
+var currentLocationIndex = 0;
 
-$(currentTownId).css('background-color', OCCUPIED_TOWN_COLOR);
+$(LOCATION_IDS[currentLocationIndex]).css('background-color', OCCUPIED_LOCATION_COLOR);
 
-function updateTownColors(townId) {
-    $(".town").css('background-color', UNOCCUPIED_TOWN_COLOR);
-    $(townId).css('background-color', OCCUPIED_TOWN_COLOR);
+function updateLocationColors(occupiedLocationId) {
+    $(".town").css('background-color', UNOCCUPIED_LOCATION_COLOR);
+    $(".road").css('background-color', UNOCCUPIED_LOCATION_COLOR);
+    $(occupiedLocationId).css('background-color', OCCUPIED_LOCATION_COLOR);
 }
 
 $("#moveCcwButton").click(function () {
-    currentTownIndex--;
-    if (currentTownIndex == -1) {
-        currentTownIndex = TOWN_IDS.length - 1;
+    currentLocationIndex--;
+    if (currentLocationIndex == -1) {
+        currentLocationIndex = LOCATION_IDS.length - 1;
     }
-    var currentTownId = TOWN_IDS[currentTownIndex];
-    updateTownColors(currentTownId);
+    updateLocationColors(LOCATION_IDS[currentLocationIndex]);
 })
 
 $("#moveCwButton").click(function () {
-    currentTownIndex++;
-    if (currentTownIndex == TOWN_IDS.length) {
-        currentTownIndex = 0;
+    currentLocationIndex++;
+    if (currentLocationIndex == LOCATION_IDS.length) {
+        currentLocationIndex = 0;
     }
-    var currentTownId = TOWN_IDS[currentTownIndex];
-    updateTownColors(currentTownId);
+    updateLocationColors(LOCATION_IDS[currentLocationIndex]);
 })
