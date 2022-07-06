82a010b6dab105f177c350e292e3774e8c030727
Joshua Rea <josh.rea@gmail.com>
2022-07-06 12:37:04 -0400

    debugged order of operations in click functions for buttons, made us start at one of the towns

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var townIds = ["#townA", "#townB", "#townD", "#townC"];
 var currentTownIndex = 0;
+var currentTownId = townIds[currentTownIndex];
+$(currentTownId).css('background-color', '#f8cf95');
 
-$(".move-button").click(function () {
+
+$("#moveCcwButton").click(function () {
+    console.log(`current town index is: ${currentTownIndex}`);
+    currentTownIndex--;
+    if (currentTownIndex == -1) {
+        currentTownIndex = 3
+    }
+    var currentTownId = townIds[currentTownIndex];
+    console.log(`current town index is: ${currentTownIndex}`);
+    $(".town").css('background-color', '#ffffff');
+    $(currentTownId).css('background-color', '#f8cf95');
+})
+
+$("#moveCwButton").click(function () {
+    console.log("I just clicked the move CW button");
+    currentTownIndex++;
+    console.log(`current town index is: ${currentTownIndex}`);
     if (currentTownIndex == 4) {
         currentTownIndex = 0
     }
     var currentTownId = townIds[currentTownIndex];
-    currentTownIndex++;
+    console.log(`current town index is: ${currentTownIndex}`);
     $(".town").css('background-color', '#ffffff');
     $(currentTownId).css('background-color', '#f8cf95');
 })
